ba2a450711a83921ccbf575f67647a66477d2c0b
james scott <j-scott@github.com>
2020-11-25 18:28:03 +0000

    new

---
+++ workshop-1/javascript.js
@@ -0,0 +1,16 @@
+
+
+
+	function pasuser(form) { 
+		// var userName = "name"; 
+		var loginPassword = "password"; 
+		//if (form.id.value== userName) 
+		{ 
+			if (form.pass.value== loginPassword ) { 
+				$('.hiddenStuff').toggleClass( "hidden", 1000, "easeInSine" );
+				$('form.well').addClass('hidden');
+				$('body').css( "background", "#fbfbfb" );
+		 	} 
+		else { alert("Wrong Password"); } } 
+		//else { alert("Wrong Username"); } 
+	} 
